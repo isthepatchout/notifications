1e456295b1c2821cdb00b32f60d7204eb661cff0
Adam Haglund <adam@haglund.dev>
2021-12-06 00:10:09 +0100

    Maintenance (#2)

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,24 @@
 import Pino from "pino"
+import { createWriteStream } from "pino-logflare"
+import pretty from "pino-pretty"
 
-export const Logger = Pino({
-  level: process.env.NODE_ENV === "development" ? "debug" : "info",
-  prettyPrint: process.env.NODE_ENV === "development",
-})
+const isDev = process.env.NODE_ENV === "development"
+
+export const Logger = Pino(
+  {
+    level: isDev ? "debug" : "info",
+    base: {
+      source: "notifications",
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  !isDev
+    ? createWriteStream({
+        apiKey: process.env.LOGFLARE_API_KEY!,
+        sourceToken: "a1bdc121-06ff-49ed-af26-5295a039d8f8",
+      })
+    : pretty({
+        colorize: true,
+        ignore: "source",
+      }),
+)
