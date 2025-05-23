87949955d5205cba44a4f0276171e4772ce58e7c
Adam <adam@haglund.dev>
2025-05-23 05:39:44 +0200

    improve logging config

--- src/logger.ts
+++ src/logger.ts
@@ -1,15 +1,15 @@
 import { pino } from "pino"
 
-const isDev = process.env.BUN_ENV === "development"
+const isProd = process.env.BUN_ENV === "production"
 
 export const Logger = pino(
   {
-    level: process.env.LOG_LEVEL ?? (isDev ? "debug" : "info"),
+    level: process.env.LOG_LEVEL ?? (isProd ? "info" : "debug"),
     base: {
       source: "notifications",
     },
   },
-  import.meta.env.DEV
+  !import.meta.env.PROD
     ? (await import("pino-pretty")).PinoPretty({ ignore: "source" })
     : undefined,
 )
